9a9519dea97842ee2354ae53927bf77b4c01b0b7
Dev singh pawar <pawarsinghdev@gmail.com>
2026-03-11 20:11:33 +0530

    add the WalletScreen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { useEffect, useState } from "react";
+// import {
+//   Keyboard,
+//   StyleSheet,
+//   Text,
+//   TouchableOpacity,
+//   View,
+// } from "react-native";
+// import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+// import { WalletScreen } from "./src/screen/WalletScreen";
+// import { SwapScreen } from "./src/screen/SwapScreen";
+// import { Ionicons } from "@expo/vector-icons";
+// import { StatusBar } from "expo-status-bar";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// export default function App() {
+//   const [activeTab, setActiveTab] = useState<"wallet" | "swap">("wallet");
+//   const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//   useEffect(() => {
+//     const showSubscription = Keyboard.addListener("keyboardDidShow", () =>
+//       setIsKeyboardVisible(true),
+//     );
+//     const hideSubscription = Keyboard.addListener("keyboardDidHide", () =>
+//       setIsKeyboardVisible(false),
+//     );
+
+//     return () => {
+//       showSubscription.remove();
+//       hideSubscription.remove();
+//     };
+//   }, []);
+
+//   return (
+//     <SafeAreaProvider>
+//       <SafeAreaView style={s.safe}>
+//         <StatusBar style="light" />
+
+//         {/* Screen Switcher */}
+//         {activeTab === "wallet" ? <WalletScreen /> : <SwapScreen />}
+
+//         {/* Bottom Tab Bar */}
+//         {!isKeyboardVisible && (
+//           <View style={s.tabBar}>
+//             <TouchableOpacity
+//               onPress={() => setActiveTab("wallet")}
+//               style={s.tab}
+//             >
+//               <Ionicons
+//                 name={activeTab === "wallet" ? "wallet" : "wallet-outline"}
+//                 size={24}
+//                 color={activeTab === "wallet" ? "#14F195" : "#6B7280"}
+//               />
+//               <Text style={[s.tabLable, activeTab === "wallet" && s.tabActive]}>
+//                 Wallet
+//               </Text>
+//             </TouchableOpacity>
+//             <TouchableOpacity
+//               onPress={() => setActiveTab("swap")}
+//               style={s.tab}
+//             >
+//               <Ionicons
+//                 name={
+//                   activeTab === "swap"
+//                     ? "swap-horizontal"
+//                     : "swap-horizontal-outline"
+//                 }
+//                 size={24}
+//                 color={activeTab === "swap" ? "#14F195" : "#6B7280"}
+//               />
+//               <Text style={[s.tabLable, activeTab === "swap" && s.tabActive]}>
+//                 Swap
+//               </Text>
+//             </TouchableOpacity>
+//           </View>
+//         )}
+//       </SafeAreaView>
+//     </SafeAreaProvider>
+//   );
+// }
+
+// const s = StyleSheet.create({
+//   safe: {
+//     flex: 1,
+//     backgroundColor: "#0D0D12",
+//   },
+//   tabBar: {
+//     flexDirection: "row",
+//     backgroundColor: "#16161D",
+//     borderTopWidth: 1,
+//     borderTopColor: "#2A2A35",
+//     paddingBottom: 8,
+//     paddingTop: 12,
+//   },
+//   tab: {
+//     flex: 1,
+//     alignItems: "center",
+//     gap: 4,
+//   },
+//   tabLable: { color: "#6B7280", fontSize: 12 },
+//   tabActive: {
+//     color: "#14F195",
+//   },
+// });
---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,55 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "#16161D",
+          borderTopColor: "#6B7280",
+        },
+        tabBarActiveTintColor: "#14F195",
+        tabBarInactiveTintColor: "#6B7280",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Wallet",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="wallet" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="swap"
+        options={{
+          title: "Swap",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="swap-horizontal" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="compass" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
